81ff197445190b06a008ef93a08fb659b0503b60
Edduardo Maturano <edu36232029@gmail.com>
2026-03-03 13:27:31 -0300

    Fix package name for AcessibilidadeRepository

--- ViiMaps/src/main/java/com/example/viimaps/repository/AcessibilidadeRepository.java
+++ ViiMaps/src/main/java/com/example/viimaps/repository/AcessibilidadeRepository.java
@@ -1,6 +1,6 @@
-package com.example.viimaps.repository;
+package com.example.viiamaps.repository;
 
-import com.example.viimaps.repository.entity.Acessibilidade;
+import com.example.viiamaps.repository.entity.Acessibilidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
